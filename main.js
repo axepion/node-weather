c13acfe02921707c7fc0a73293653fd51d9ae838
axepion <axepion7250@gmail.com>
2023-07-11 00:39:28 +0300

    builder url

--- main.js
+++ main.js
@@ -3,35 +3,66 @@
 const http = require('node:http');
 const { keyAPI } = require('./config.js');
 
+class MetaCity {
+    constructor(...args) {
+        this._url = 'http://api.openweathermap.org/data/2.5/weather?';
+        
+        this._parameters = {
+            name: 'q=',
+            api: `&appid=${keyAPI}`,
+        };
 
+        if (args.length > 3 || args.length === 0) throw Error('Invalid number of arguments');
 
-class BuildCity {
-    constructor(name, latitude = null, longitude = null) {
-        this.url = 'https://api.openweathermap.org';
-        this.name = name;
-        this.latitude = latitude;
-        this.longitude = longitude;
-        Object.defineProperty(this, this.url, {
-            enumerable: false,
-            configurable: false,
-            writable: false,
-        });
-    }
+        if (typeof args[0] === 'string') {
+            this.name = args[0]
+            this.url = this._url + this._parameters['name'] + args[0] + this._parameters['api'];
+        }
+        else {
+            if (typeof args[0] === 'number' && typeof args[1] === 'number') {
+                this.latitude = args[0];
+                this.longitude = args[1];
+                this.url = this._url + `lat=${this.latitude}&lon=${this.longitude}` + this._parameters['api'];
+            } else {
+                throw Error('Incorrect use of the class BuildCity')
+            };
+        }
 
-    buildFromName(name) {
-        const url = 
-        return new City(url)
-    }
-}
+        
+    };
+};
 
-class City {
-    constructor(url) {
-        this.url = url;
-    }
-}
+const fetchWeather = (url) => new Promise((resolve, reject) => {
+    http.get(url, (res) => {
+        const code = res.statusCode;
+        if (code !== 200) return reject(new Error(`HTTP status code ${code}`))
 
-http.get(`http://api.openweathermap.org/data/2.5/weather?units=metric&lat=58.35&lon=49.39&exclude=current&appid=${keyAPI}`, (res) => {   
-    res.on('data', data => {
-        console.log(data.toString());
+        res.on('error', reject)
+
+        const chunks = [];
+        res.on('data', (chunk) => {
+            chunks.push(chunk);
+        })
+
+        res.on('end', () => {
+            const json = Buffer.concat(chunks).toString();
+            try {
+                const object = JSON.parse(json);
+                resolve(object);
+            } catch (error) {
+                return reject(error);
+            }
+        })
+        
     })
-})
+});
+
+const getWeather = (metaCity) => {
+    
+    fetchWeather(url);
+}
+
+
+const kirov = new MetaCity('Kirov');
+
+console.log(kirov);
