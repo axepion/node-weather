775a186323fef60eeb0366182ae1171cfd282907
axepion <axepion7250@gmail.com>
2023-07-10 17:58:12 +0300

    create Classes

--- main.js
+++ main.js
@@ -3,18 +3,34 @@
 const http = require('node:http');
 const { keyAPI } = require('./config.js');
 
-const url = `https://api.openweathermap.org`;
-const options = {
-    hostname: 'http://api.openweathermap.org',
-    path: `/data/2.5/weather?lat=33.44&lon=-94.04&exclude=current&appid=${keyAPI}/`,
-    method: 'PATH',
-    headers: {
-        'Content-Type': 'application/json',
+
+
+class BuildCity {
+    constructor(name, latitude = null, longitude = null) {
+        this.url = 'https://api.openweathermap.org';
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        Object.defineProperty(this, this.url, {
+            enumerable: false,
+            configurable: false,
+            writable: false,
+        });
+    }
+
+    buildFromName(name) {
+        const url = 
+        return new City(url)
     }
 }
 
-http.get(`http://api.openweathermap.org/data/2.5/weather?units=metric&lat=58.35&lon=49.39&exclude=current&appid=${keyAPI}`, (res) => {
-    const data = [];    
+class City {
+    constructor(url) {
+        this.url = url;
+    }
+}
+
+http.get(`http://api.openweathermap.org/data/2.5/weather?units=metric&lat=58.35&lon=49.39&exclude=current&appid=${keyAPI}`, (res) => {   
     res.on('data', data => {
         console.log(data.toString());
     })
